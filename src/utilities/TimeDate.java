dce5258ccb50960fb2ba20ea4a3e46482beec64b
augustalex <augustalexandersson@gmail.com>
2016-11-14 14:10:16 +0100

    Added A LOT of documenting javadoc <3

--- src/utilities/TimeDate.java
+++ src/utilities/TimeDate.java
@@ -4,5 +4,9 @@ package utilities;
  * Specifies a specific time and date.
  */
 public class TimeDate {
+    private String dateString;
 
+    public TimeDate(String dateString){
+        this.dateString = dateString;
+    }
 }
---
+++ src/views/configureTournament/ConfigureTournamentViewController.java
@@ -0,0 +1,69 @@
+package views.configureTournament;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import metaInformation.MetaInformation;
+import metaInformation.TournamentMetaInformation;
+import tournament.tournamentConfiguration.ITournamentConfiguration;
+import tournament.tournamentConfiguration.TournamentConfiguration;
+import tournament.tournamentConfiguration.TournamentConfigurationFactory;
+import tournament.tournamentStyle.TournamentStyle;
+import tournament.tournamentStyle.TreeTournamentStyle;
+import utilities.TimeDate;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Patric on 2016-11-14.
+ */
+public class ConfigureTournamentViewController implements Initializable {
+
+    @FXML
+    private VBox newTournamentContainer;
+
+    @FXML
+    private HBox tournamentHBox;
+
+    @FXML
+    private ComboBox tournamentComboBox;
+
+    @FXML
+    private TextField tournamentName;
+
+    @FXML
+    private TextArea tournamentDesc;
+
+    @FXML
+    private TextField tournamentDate;
+
+    @FXML
+    private Button buttonTournamentSubmit;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        buttonTournamentSubmit.setOnAction(e -> {
+
+            TournamentStyle style = new TreeTournamentStyle();
+            TimeDate time = new TimeDate();
+
+            MetaInformation metaInformation =
+                    new TournamentMetaInformation()
+                    .setStartDate(time)
+                    .setName(tournamentName.getText())
+                    .setDescription(tournamentDesc.getText());
+
+            ITournamentConfiguration configuration =
+                    TournamentConfigurationFactory
+                    .newTournamentConfiguration()
+                    .setTournamentStyle(style)
+                    .setMetaInformation((TournamentMetaInformation)metaInformation);
+        });
+    }
+}
