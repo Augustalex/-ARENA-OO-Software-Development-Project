aa83323d7aec6b9c60cb4c29c51d8dfa92e1226b
augustalex <augustalexandersson@gmail.com>
2016-11-15 09:19:04 +0100

    Fixed problems.

--- src/views/configureTournament/ConfigureTournamentViewController.java
+++ src/views/configureTournament/ConfigureTournamentViewController.java
@@ -58,15 +58,22 @@ public class ConfigureTournamentViewController implements Initializable {
 
         DimensionBinder.bindWidthToPercentageOfContainer(createTournamentContent, 1, createTournamentContainer);
 
-        ITournamentConfiguration configuration = TournamentConfigurationFactory.newTournamentConfiguration();
-
         buttonTournamentSubmit.setOnAction(e -> {
+            ITournamentConfiguration configuration = this.newConfigurationFromFormData();
             if(configuration.isValid())
-            System.out.println(configuration.toString());
+                configuration.submit();
+            else
+                System.out.println("All fields are not filled in correctly!");
         });
     }
 
-    private void fillConfigurationWithFormData(ITournamentConfiguration tournamentConfiguration){
+    /**
+     * Takes all input from the view objects and puts the in a new TournamentConfiguration object.
+     *
+     * This object is not guaranteed to be valid, and some fields may be empty.
+     * @return a new ITournamentConfiguration
+     */
+    private ITournamentConfiguration newConfigurationFromFormData(){
 
         ITournamentStyle style = TournamentStyleFactory.newTournamentStyle();
         TimeDate time = new TimeDate(tournamentDate.getText());
@@ -83,5 +90,6 @@ public class ConfigureTournamentViewController implements Initializable {
                         .setTournamentStyle(style)
                         .setMetaInformation((TournamentMetaInformation)metaInformation);
 
+        return configuration;
     }
 }
