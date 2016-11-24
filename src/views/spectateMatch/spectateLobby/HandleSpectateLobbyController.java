06ff8c622939363a28566980d38f84e9b289f0fb
Björn <s132063@student.hb.se>
2016-11-24 11:50:56 +0100

    spectatorshit

--- src/views/spectateMatch/spectateLobby/HandleSpectateLobbyController.java
+++ src/views/spectateMatch/spectateLobby/HandleSpectateLobbyController.java
@@ -1,10 +1,12 @@
 package views.spectateMatch.spectateLobby;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -27,13 +29,14 @@ public class HandleSpectateLobbyController implements Initializable{
     private Label test;
 
     @FXML
-    private ListView listtest;
-
-    @FXML
     private TableView tabletest;
-    ObservableList observableList = FXCollections.observableArrayList("chocolate", "salmon", "gold", "coral", "darkorchid",
-            "darkgoldenrod", "lightsalmon", "black", "rosybrown", "blue",
-            "blueviolet", "brown");
+    ObservableList observableList = FXCollections.observableArrayList(
+            new Person("ProLeague", "BestOfFiveUltimate", "Yoda Vs Vader"),
+            new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+            new Person("Ethan", "Williams", "ethan.williams@example.com"),
+            new Person("Emma", "Jones", "emma.jones@example.com"),
+            new Person("Michael", "Brown", "michael.brown@example.com")
+    );
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         configureView();
@@ -46,36 +49,52 @@ public class HandleSpectateLobbyController implements Initializable{
     }
     public void setTableView(){
         tabletest.setEditable(true);
-        TableColumn firstNameCol = new TableColumn("First Name");
-        TableColumn lastNameCol = new TableColumn("Last Name");
-        TableColumn emailCol = new TableColumn("Email");
-        tabletest.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
+        TableColumn leagueNameCol = new TableColumn("League Name");
+        TableColumn tournamentNameCol = new TableColumn("Tournament Name");
+        TableColumn matchGamesCol = new TableColumn("MatchGame");
+        leagueNameCol.setMinWidth(200);
+        leagueNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+        tournamentNameCol.setMinWidth(200);
+        tournamentNameCol.setCellValueFactory( new PropertyValueFactory<Person, String>("lastName"));
+        matchGamesCol.setMinWidth(200);
+        matchGamesCol.setCellValueFactory( new PropertyValueFactory<Person, String>("email"));
+        tabletest.setItems(observableList);
+        tabletest.getColumns().addAll(leagueNameCol, tournamentNameCol, matchGamesCol);
     }
-    private void setListView()
-    {
-        listtest.setItems(observableList);
-        listtest.setCellFactory(new Callback<ListView<String>,
-                        ListCell<String>>() {
-            @Override
-            public ListCell<String> call(ListView<String> listtest) {
-                return new Cell();
-            };
-        });
-    }
-    static class Cell extends ListCell<String> {
-        @Override
-        public void updateItem(String item, boolean empty) {
-            super.updateItem(item, empty);
-            Label rect1 = new Label();
-            Label rect2 = new Label();
-            HBox box = new HBox();
-            if (item != null) {
-                rect1.setText(item);
-                rect2.setText(" YoloSwag");
-                box.getChildren().setAll(rect1, rect2);
-                setGraphic(box);
-            }
+    public static class Person {
 
+        private final SimpleStringProperty firstName;
+        private final SimpleStringProperty lastName;
+        private final SimpleStringProperty email;
+
+        private Person(String fName, String lName, String email) {
+            this.firstName = new SimpleStringProperty(fName);
+            this.lastName = new SimpleStringProperty(lName);
+            this.email = new SimpleStringProperty(email);
+        }
+
+        public String getFirstName() {
+            return firstName.get();
+        }
+
+        public void setFirstName(String fName) {
+            firstName.set(fName);
+        }
+
+        public String getLastName() {
+            return lastName.get();
+        }
+
+        public void setLastName(String fName) {
+            lastName.set(fName);
+        }
+
+        public String getEmail() {
+            return email.get();
+        }
+
+        public void setEmail(String fName) {
+            email.set(fName);
         }
     }
 }
