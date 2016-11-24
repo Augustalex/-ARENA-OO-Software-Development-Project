542bb14e2e2779f0067248ef98b19e7150a03190
Simon Kevin <zimme95@gmail.com>
2016-11-24 11:09:55 +0100

    AvailibeGamesList -> AvailibleMatchList

---
+++ src/spectate/AvailibleMatchList.java
@@ -0,0 +1,24 @@
+package spectate;
+
+import tournament.match.IMatch;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by Simon on 24/11/2016.
+ */
+public class AvailibleMatchList implements Serializable{
+
+    ArrayList<IMatch> availibleMatches = new ArrayList<>();
+
+    public AvailibleMatchList(){
+
+    }
+
+    public void viewAvailibeMatches(){
+        for(int i = 0; i < availibleMatches.size(); i++){
+            System.out.println(availibleMatches.get(i).getGameInformation().getGameName());
+        }
+    }
+}
