b46c28a16c1782ce285aecf585fdd7b3677a1f70
augustalex <augustalexandersson@gmail.com>
2016-11-29 00:16:07 +0100

    Added basic stream modules. Not for "production" use, but runnable and testable.

---
+++ CompressionUtils/src/utils/CompressionUtils.java
@@ -0,0 +1,46 @@
+package utils;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+public class CompressionUtils {
+
+    public static byte[] compress(byte[] data) throws IOException {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        deflater.finish();
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer); // returns the generated code... index
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+//        System.out.println("Original: " + data.length / 1024 + " Kb");
+//        System.out.println("Compressed: " + output.length / 1024 + " Kb");
+        return output;
+    }
+    public static byte[] decompress(byte[] data) throws IOException, DataFormatException {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!inflater.finished()) {
+            int count = inflater.inflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.close();
+        byte[] output = outputStream.toByteArray();
+//        System.out.println("Original: " + data.length);
+//        System.out.println("Compressed: " + output.length);
+        return output;
+    }
+}
