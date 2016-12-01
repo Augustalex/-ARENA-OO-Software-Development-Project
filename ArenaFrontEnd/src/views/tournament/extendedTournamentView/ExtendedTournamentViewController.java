a775df4d4fedd198d82e1774745c86577b6e0531
Simon Kevin <zimme95@gmail.com>
2016-12-01 15:45:02 +0100

    extendedTournamentView påbörjad

---
+++ ArenaFrontEnd/src/views/tournament/extendedTournamentView/ExtendedTournamentViewController.java
@@ -0,0 +1,33 @@
+package views.tournament.extendedTournamentView;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import views.FXMLViewController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Simon on 01/12/2016.
+ */
+public class ExtendedTournamentViewController extends FXMLViewController {
+
+    @FXML
+    private Label TournamentName;
+
+    @FXML
+    private Label Matches;
+
+    @FXML
+    private
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public void closeView() {
+        System.out.println("LOL");
+    }
+}
